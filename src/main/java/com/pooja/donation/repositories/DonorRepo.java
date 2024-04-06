09b93bbd65217c1841415af6606e455292aa0722
Pooja <poojaswain113@gmail.com>
2024-04-06 15:20:42 +0530

    Uploading project.

---
+++ src/main/java/com/pooja/donation/repositories/DonorRepo.java
@@ -0,0 +1,9 @@
+//package com.pooja.donation.repositories;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import com.pooja.donation.entities.Donor;
+//
+//public interface DonorRepo extends JpaRepository<Donor, Integer > {
+//
+//}
