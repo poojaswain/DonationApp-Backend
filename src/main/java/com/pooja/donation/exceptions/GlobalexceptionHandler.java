cf10f5ac49c003b00d0cc9fdcc69218c01fb1205
Abhishek Gupta <abhishek.work07@gmail.com>
2024-04-14 13:33:38 +0530

    Added new entities for Donar & Receiver.

--- src/main/java/com/pooja/donation/exceptions/GlobalexceptionHandler.java
+++ src/main/java/com/pooja/donation/exceptions/GlobalexceptionHandler.java
@@ -18,24 +18,22 @@ import com.pooja.donation.payloads.ApiResponse;
 public class GlobalexceptionHandler {
 
 	@ExceptionHandler(ResourceNotFoundException.class)
-	public ResponseEntity<ApiResponse> resourceNotFoundExceptionHandler(ResourceNotFoundException ex){
-		String message= ex.getMessage();
-		ApiResponse apiResponse= new ApiResponse(message, false);
+	public ResponseEntity<ApiResponse> resourceNotFoundExceptionHandler(ResourceNotFoundException ex) {
+		String message = ex.getMessage();
+		ApiResponse apiResponse = new ApiResponse(message, false);
 		return new ResponseEntity<ApiResponse>(apiResponse, HttpStatus.NOT_FOUND);
 	}
-	
+
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<Map<String, String>> handleMethodArgsNotValidException(MethodArgumentNotValidException ex)
-	{
-		Map<String, String> resp=new HashMap<>();
-		ex.getBindingResult().getAllErrors().forEach((error)->{
-			String fieldName= ((FieldError)error).getField();
+	public ResponseEntity<Map<String, String>> handleMethodArgsNotValidException(MethodArgumentNotValidException ex) {
+		Map<String, String> resp = new HashMap<>();
+		ex.getBindingResult().getAllErrors().forEach((error) -> {
+			String fieldName = ((FieldError) error).getField();
 			String message = error.getDefaultMessage();
 			resp.put(fieldName, message);
 		});
-		
-		return new ResponseEntity<Map<String,String>>(resp,HttpStatus.BAD_REQUEST);
+
+		return new ResponseEntity<Map<String, String>>(resp, HttpStatus.BAD_REQUEST);
 	}
-	
-	
+
 }
